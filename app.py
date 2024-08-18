3d437277e6952bfddef7f6fcae18f3ffeb52b000
rodrigoskohl <rodrigokohl1@yahoo.com>
2024-08-17 22:32:26 -0300

    criada api de processamento de dados

--- app.py
+++ app.py
@@ -5,19 +5,37 @@ from dotenv import load_dotenv
 from bson import ObjectId
 import secrets
 from pymongo.errors import ServerSelectionTimeoutError
+from data_process import fetch_data, process_and_save_data
+
 
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
 
-app = Flask(__name__)
-
 # Configurações do Flask
+app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)  # Gera uma nova chave secreta a cada inicialização
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
+#  URI do MongoDB
+mongo_ip = os.getenv('MONGO_IP')
+mongo_port = os.getenv('MONGO_PORT')
+mongo_user = os.getenv('MONGO_USER')
+mongo_password = os.getenv('MONGO_PASSWORD')
+mongo_dbname = os.getenv('MONGO_DBNAME')
+mongo_collection = os.getenv('MONGO_COLLECTION')
+# Verifica se todas as variáveis de ambiente estão definidas
+if not all([mongo_ip, mongo_port, mongo_user, mongo_password, mongo_dbname, mongo_collection]):
+    raise ValueError("Uma ou mais variáveis de ambiente não estão definidas.")
+
+mongo_uri = f"mongodb://{mongo_user}:{mongo_password}@{mongo_ip}:{mongo_port}/{mongo_dbname}"
+
+app.config['MONGO_URI'] = mongo_uri
+
+# Inicializa o PyMongo com o construtor
 mongo = PyMongo(app)
+
+# Configura a coleção
 db = mongo.db
-collection = db[os.getenv('MONGO_COLLECTION')]  # Configura a coleção
+collection = db[mongo_collection]
 
 def test_mongo_connection():
     uri = app.config['MONGO_URI']
@@ -28,6 +46,41 @@ def test_mongo_connection():
     except ServerSelectionTimeoutError as e:
         print(f"Erro de conexão com o MongoDB ({uri}): {e}")
         exit(1)  # Encerra o programa com código de erro
+
+
+# Função para salvar os dados no MongoDB
+def save_to_mongo(data):
+    """Salva os dados no MongoDB."""
+    try:
+        collection.insert_many(data)
+        print("Os dados combinados foram salvos no MongoDB.")
+    except Exception as e:
+        print(f"Erro ao salvar os dados no MongoDB: {e}")
+
+# Verifica se a coleção existe e executa o fetch e o processamento dos dados se necessário
+def fetch_and_process_data_if_collection_missing():
+    """Executa o fetch e o processamento dos dados se a coleção não existir."""
+    if mongo_collection not in db.list_collection_names():
+        print(f"A coleção '{mongo_collection}' não existe. Executando fetch e processamento de dados.")
+        dict_1, dict_2, dict_3 = fetch_data()
+        if dict_1 and dict_2 and dict_3:
+            # Processa os dados e obtém o cleaned_data
+            cleaned_data = process_and_save_data(dict_1, dict_2, dict_3)
+            # Salva os dados processados no MongoDB
+            save_to_mongo(cleaned_data)
+        else:
+            print("Erro ao buscar os dados. O processamento não será executado.")
+            exit(1)  # Encerra o programa com código de erro
+    else:
+        print(f"A coleção '{collection}' já existe. Fetch e processamento de dados não são necessários.")
+
+
+def serialize_document(document):
+    """Converte um documento MongoDB para um formato JSON serializável."""
+    if document is None:
+        return None
+    document['_id'] = str(document['_id'])  # Converte ObjectId para string
+    return document
 
 @app.route('/')
 def index():
@@ -46,21 +99,29 @@ def index():
 @app.route('/data', methods=['GET'])
 def get_data():
     data = list(collection.find({}))
-    return jsonify(data)
+    # Aplica a função de serialização a cada documento
+    serialized_data = [serialize_document(doc) for doc in data]
+    # Envolve os dados em um objeto com a chave 'data'
+    return jsonify({"data": serialized_data})
+
+
+
+@app.route('/data/<string:id>', methods=['GET'])
+def get_single_data(id):
+    try:
+        # Busque pelo campo 'id' em vez de '_id' do MongoDB
+        data = collection.find_one({"id": id})
+        serialized_data = serialize_document(data)
+        return jsonify({"data": serialized_data}) if serialized_data else ("Not Found", 404)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 @app.route('/data', methods=['POST'])
 def add_data():
     json_data = request.json
     result = collection.insert_one(json_data)
     return jsonify({"inserted_id": str(result.inserted_id)}), 201
-
-@app.route('/data/<string:id>', methods=['GET'])
-def get_single_data(id):
-    try:
-        data = collection.find_one({"_id": ObjectId(id)})
-        return jsonify(data) if data else ("Not Found", 404)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 @app.route('/data/<string:id>', methods=['PUT'])
 def update_data(id):
@@ -87,4 +148,5 @@ def delete_data(id):
 
 if __name__ == '__main__':
     test_mongo_connection()  # Testa a conexão antes de iniciar o servidor
+    fetch_and_process_data_if_collection_missing() # Executa o fetch e o processamento se a coleção não existir
     app.run(debug=True)
---
+++ data_process.py
@@ -0,0 +1,168 @@
+import requests
+import xmltodict
+import json
+from bson import ObjectId
+
+
+# Função para extrair campos comuns de uma entrada
+def extract_common_fields(entry):
+    return {
+        "id": entry.get("id"),
+        "contact_id": entry.get("contact_id"),
+        "status": entry.get("status"),
+        "date_submitted": entry.get("date_submitted"),
+        "session_id": entry.get("session_id"),
+        "language": entry.get("language"),
+        "date_started": entry.get("date_started"),
+        "ip_address": entry.get("ip_address"),
+        "referer": entry.get("referer"),
+        "user_agent": entry.get("user_agent"),
+        "country": entry.get("country"),
+    }
+
+# Função para buscar os dados dos três modelos
+def fetch_data():
+    try:
+        # Carregar os dados dos três modelos
+        response_1 = requests.get('https://numera-case.web.app/v1/survey/1/answers')
+        response_1.raise_for_status()  # Verifica se a requisição foi bem-sucedida
+        dict_1 = response_1.json()
+
+        response_2 = requests.get('https://numera-case.web.app/v1/survey/2/answers')
+        response_2.raise_for_status()
+        dict_2 = response_2.json()
+
+        response_3 = requests.get('https://numera-case.web.app/v1/survey/3/answers')
+        response_3.raise_for_status()
+        dict_3 = xmltodict.parse(response_3.text)
+
+        # Retornar os dados apenas se todas as requisições forem bem-sucedidas
+        return dict_1, dict_2, dict_3
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro na requisição: {e}")
+        return None, None, None
+    except ValueError as e:
+        print(f"Erro ao processar os dados: {e}")
+        return None, None, None
+
+# Função para padronizar e salvar os dados
+def process_and_save_data(dict_1, dict_2, dict_3):
+
+    # Mapeia perguntas para IDs para usar na estrutura 2
+    question_id_map = {}
+
+    # Processando a primeira estrutura e criando o mapeamento
+    for entry in dict_1["data"]:
+        for question_id, question_data in entry["survey_data"].items():
+            question_text = question_data.get("question", "")
+            question_id_map[question_text] = question_id
+
+    # Processando a terceira estrutura e adicionando ao mapeamento
+    for entry in dict_3["survey_answer"]["data"]["item"]:
+        for survey_item in entry["survey_data"]["item"]:
+            question_text = survey_item.get("question", "")
+            question_id = survey_item["id"]
+            if question_text not in question_id_map:
+                question_id_map[question_text] = question_id
+
+    # Estrutura Final
+    combined_data = {}
+
+    # Processando a primeira estrutura e ajustando o formato
+    for entry in dict_1["data"]:
+        combined_data[entry["id"]] = {
+            **extract_common_fields(entry), 
+            "survey_data": {}
+        }
+        
+        for question_id, question_data in entry["survey_data"].items():
+            survey_data_entry = {
+                "question": question_data.get("question", None),
+                "answer": question_data.get("answer", None),
+                "comments": question_data.get("comments", None)
+            }
+            
+            combined_data[entry["id"]]["survey_data"][question_id] = survey_data_entry
+
+    # Processando a segunda estrutura e adicionando IDs
+    for entry in dict_2["data"]:
+        survey_id = entry["id"]
+        
+        # Inicializa a entrada no dicionário combinado, caso não exista
+        if survey_id not in combined_data:
+            combined_data[survey_id] = {
+                **extract_common_fields(entry), 
+                "survey_data": {}
+            }
+        
+        # Itera sobre as perguntas e respostas
+        for question, answer in entry.items():
+            if question not in ["id", "contact_id", "status", "date_submitted", "session_id", "language", "date_started", "ip_address", "referer", "user_agent", "country"]:
+                question_id = question_id_map.get(question, question)
+                try:
+                    # Tenta carregar a resposta como JSON, se possível
+                    answer_data = json.loads(answer)
+                    if isinstance(answer_data, list):
+                        # Ajusta o formato da resposta para o padrão desejado
+                        formatted_answer = [{"id": item["id"], "option": item["option"], "rank": item["rank"]} for item in answer_data]
+                        combined_data[survey_id]["survey_data"][question_id] = {
+                            "answer": formatted_answer,
+                            "comments": entry.get(f"{question}_comments", None)  # Garante que comments seja criado  se não existir
+                        }
+                    else:
+                        combined_data[survey_id]["survey_data"][question_id] = {
+                            "question": question,
+                            "answer": answer_data,
+                            "comments": entry.get(f"{question}_comments", None)  # Garante que comments seja criado  se não existir
+                        }
+                except json.JSONDecodeError:
+                    # Se a resposta não for JSON, usa como está
+                    combined_data[survey_id]["survey_data"][question_id] = {
+                        "question": question,
+                        "answer": answer,
+                        "comments": entry.get(f"{question}_comments", None)  # Garante que comments seja criado  se não existir
+                    }
+
+    # Processando a terceira estrutura e adicionando ao dicionário combinado
+    for entry in dict_3["survey_answer"]["data"]["item"]:
+        survey_id = entry["id"]
+        if survey_id not in combined_data:
+            combined_data[survey_id] = {
+                **extract_common_fields(entry), 
+                "survey_data": {}
+            }
+        
+        for survey_item in entry["survey_data"]["item"]:
+            answer = survey_item.get("answer", None)
+            if isinstance(answer, dict):
+                answer = answer.get("item", answer)
+            
+            # Cria o dicionário para a entrada de dados
+            survey_data_entry = {
+                "question": survey_item["question"],
+                "answer": answer,
+                "comments": survey_item.get("comments", None)  # Garante que comments seja criado  se não existir
+            }
+            
+            combined_data[survey_id]["survey_data"][survey_item["id"]] = survey_data_entry
+
+        # Tratamento dos dados para inserir no MongoDB
+        cleaned_data = []
+        for k, v in combined_data.items():
+            try:
+                # Tenta converter o valor de k para ObjectId
+                object_id = ObjectId(k)
+            except Exception:
+                # Se a conversão falhar, gere um novo ObjectId
+                object_id = ObjectId()
+            
+            # Cria um novo documento com _id como ObjectId e mantém o campo 'id'
+            document = {
+                "_id": object_id,  # Converte o valor de k para ObjectId
+                "id": k,  # Mantém o campo 'id' original
+                **{key: value for key, value in v.items()}  # Mantém os outros campos
+            }
+            cleaned_data.append(document)
+    
+    return cleaned_data
