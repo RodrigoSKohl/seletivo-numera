5a55c824ceb46a6df0b75c2b895cc1be50065b15
RodrigoSKohl <rodrigokohl1@yahoo.com>
2024-08-24 05:13:00 -0300

    update multi worker production gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,9 +2,16 @@ import os
 
 # Obtém a porta da variável de ambiente, com valor padrão 8000 se não estiver definida
 web_port = os.getenv('WEB_PORT', '8000')
-
 bind = f"0.0.0.0:{web_port}"
-workers = 4
-timeout = 120
 loglevel = "info"
 preload = True  # Precarrega a aplicação antes de fork
+
+worker_class = 'gevent'
+workers = 4
+worker_connections = 250
+max_requests = 500
+timeout = 120
+graceful_timeout = 30
+limit_request_line = 4094
+limit_request_fields = 100
+limit_request_field_size = 8190
